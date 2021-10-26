971fa9f59e6283ebedbbbbe1baf6f3f82462a63f
numa-create <k.numata0815@gmail.com>
2021-10-26 12:22:44 +0900

    Rails課題02

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get '/users', to: 'users#index'
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/users/:id/edit', to: 'users#edit'
+
+  # 以下のルーティングを追加
+  patch '/users/:id', to: 'users#update'
+
+  delete '/users/:id', to: 'users#destroy'
+end
